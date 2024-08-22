64be97a00773dc81e597df47d42188d7b2d06ee4
Mehmet Karahan Çetinkaya <71176654+karahanOS@users.noreply.github.com>
2024-08-22 16:30:26 +0300

    Update DIM_Customer.sql
    comments added

--- DIM_Customer.sql
+++ DIM_Customer.sql
@@ -3,6 +3,6 @@ SELECT  c.CustomerID
 	   ,c.age
 	   ,g.Country
 	   ,g.City
-FROM customers AS c
-LEFT JOIN geography AS g
-ON c.GeographyID = g.GeographyID
+FROM customers AS c -- Alised table
+LEFT JOIN geography AS g -- Alised table
+ON c.GeographyID = g.GeographyID -- Tables merged 
