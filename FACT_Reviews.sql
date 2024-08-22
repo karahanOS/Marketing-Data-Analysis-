630979fd4a22ee63d66e2360dccbf229add8233e
Mehmet Karahan Çetinkaya <71176654+karahanOS@users.noreply.github.com>
2024-08-22 16:10:01 +0300

    Update FACT_Reviews.sql
    comments added to code

--- FACT_Reviews.sql
+++ FACT_Reviews.sql
@@ -1,8 +1,11 @@
+-- Cleaned and Transformed Reviews table
 SELECT [ReviewID]
       ,[CustomerID]
       ,ProductID
       ,FORMAT(CONVERT(DATE,[ReviewDate]),'dd-MM-yyyy') AS  ReviewDate 
+      -- Date format changed from yyyy-MM-dd to dd-MM-yyyy
       ,Rating
       ,REPLACE([ReviewText], '  ', ' ')  AS ReviewText 
+      -- ReviewText column had double spacing in some data those are cleaned by using REPLACE function 
   FROM [PortfolioProject_MarketingAnalytics].[dbo].[customer_reviews]
   ORDER BY Rating DESC
