3e7732823e316202fb3a944cf08bcebb16da92da
Mehmet Karahan Çetinkaya <71176654+karahanOS@users.noreply.github.com>
2024-08-22 16:28:37 +0300

    Update DIM_Product.sql
    added comments

--- DIM_Product.sql
+++ DIM_Product.sql
@@ -1,10 +1,9 @@
-SELECT TOP(10) ProductID
+SELECT   ProductID
 	,ProductName
-	--,Category
 	,Price
 	,CASE
 		WHEN Price < 50  THEN 'LOW'
 		WHEN Price > 200 THEN 'HIGH'
 		ELSE 'MEDIUM'
-	END AS PriceCategory 	
+	END AS PriceCategory -- Categorized prices
 FROM  products
